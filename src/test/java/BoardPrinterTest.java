60421199db468f3921906e81d5da4f221eae32f6
Adrien Turmo <adrien.turmo@gmail.com>
2017-08-01 15:45:47 +0200

    Feature Move-Up : add a test for boardPrinter

--- src/test/java/BoardPrinterTest.java
+++ src/test/java/BoardPrinterTest.java
@@ -1,7 +1,45 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.inOrder;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BoardPrinterTest {
+
+    private Board board;
+    private BoardPrinter boardPrinter;
+
+    @Mock
+    private Printer printer;
+
+    @Before
+    public void setUp() throws Exception {
+        board = new Board(4,4);
+        boardPrinter = new BoardPrinter(printer);
+    }
+
+    @Test
+    public void should_print_the_board() throws Exception {
+        board.put2AtPosition(0, 0);
+
+        boardPrinter.printBoard(board);
+
+        InOrder inOrder = inOrder(printer);
+        inOrder.verify(printer).print("---------");
+        inOrder.verify(printer).print("| | | | |");
+        inOrder.verify(printer).print("---------");
+        inOrder.verify(printer).print("| | | | |");
+        inOrder.verify(printer).print("---------");
+        inOrder.verify(printer).print("| | | | |");
+        inOrder.verify(printer).print("---------");
+        inOrder.verify(printer).print("|2| | | |");
+        inOrder.verify(printer).print("---------");
+
+    }
 
 }
